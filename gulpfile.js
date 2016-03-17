fd8fd3e5978f62c0d6d72e178c49a22afc8a9da7
James Benner <james.benner@gmail.com>
2016-03-16 22:47:41 -0400

    Changed `src` directory to `lib`.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ let jscs   = require('gulp-jscs');
 let jshint = require('gulp-jshint');
 
 gulp.task('js', () => {
-    return gulp.src('src/**/*.js')
+    return gulp.src('lib/**/*.js')
         .pipe(jscs())
         .pipe(jscs.reporter())
         .pipe(jshint())
