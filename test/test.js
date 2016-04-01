1602846a1161918f596c0c1ce7c92c0876af2a41
James Benner <james.benner@gmail.com>
2016-03-31 21:58:33 -0400

    Fixed tests.

--- test/test.js
+++ test/test.js
@@ -1,28 +1,47 @@
 'use strict';
 
-var assert     = require('assert');
-var gutil      = require('gulp-util');
-var handlebars = require('handlebars');
-var stachio    = require('../');
+const assert     = require('assert');
+const expect     = require('chai').expect;
+const gutil      = require('gulp-util');
+const handlebars = require('handlebars');
+const stachio    = require('../');
 
-it('should ', function (cb) {
-    var template = '<h1>Unicorns are... {{something}}!</h1>';
-    var context  = { something: 'awesome' };
-    var stream   = stachio(context);
-
-    stream.on('data', function (file) {
-        var renderer = handlebars.compile(template);
-        assert.equal(file.relative, 'file.ext');
-        assert.equal(file.contents.toString(), renderer(context));
+describe('Gulp Stachio', () => {
+    let template  = '<h1>Unicorns are... {{something}}!</h1>';
+    let context   = {something: 'awesome'};
+    let vinylFile = new gutil.File({
+        base: __dirname,
+        path: __dirname + '/file.hbs',
+        contents: new Buffer(template)
     });
 
-    stream.on('end', cb);
+    it('should compile a template with context', (cb) => {
+        let stream = stachio(context);
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.ext',
-        contents: new Buffer(template)
-    }));
+        stream.write(vinylFile);
 
-    stream.end();
+        stream.on('data', function (file) {
+            let html = handlebars.compile(template)(context);
+
+            expect(file.contents.toString()).to.equal(html);
+        });
+
+        stream.on('end', cb);
+
+        stream.end();
+    });
+
+    it('should change the file extension to "html"', (cb) => {
+        let stream = stachio(context);
+
+        stream.write(vinylFile);
+
+        stream.on('data', (file) => {
+            expect(file.relative).to.equal('file.html');
+        });
+
+        stream.on('end', cb);
+
+        stream.end();
+    });
 });
